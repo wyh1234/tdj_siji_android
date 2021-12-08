be38eafbc4e361a1ee7923b315081fefe856e1d2
hzz <1759498966@qq.com>
2021-12-08 14:21:07 +0800

    [修改]司机端增加水印工具类

--- app/src/main/java/com/tdj_sj_webandroid/utils/BitmapTools.java
+++ app/src/main/java/com/tdj_sj_webandroid/utils/BitmapTools.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class BitmapTools {
     /**
      * 图片按比例大小压缩方法
+     *
      * @param srcPath （根据路径获取图片并压缩）
      * @return
      */
@@ -80,6 +81,50 @@ public class BitmapTools {
         }
         return f;
 
+    }
+
+    /**
+     * 图片按比例大小压缩方法
+     *
+     * @param srcPath （根据路径获取图片并压缩）
+     * @return 返回newOpts和bitMap两个对象
+     */
+    public static ReturnObject getImageTwo(String srcPath) {
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        // 开始读入图片，此时把options.inJustDecodeBounds 设回true了
+        newOpts.inJustDecodeBounds = true;
+        Bitmap bitmap = BitmapFactory.decodeFile(srcPath, newOpts);// 此时返回bm为空
+        newOpts.inJustDecodeBounds = false;
+        int w = newOpts.outWidth;
+        int h = newOpts.outHeight;
+        // 现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
+        float hh = 800f;// 这里设置高度为800f
+        float ww = 480f;// 这里设置宽度为480f
+        // 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;// be=1表示不缩放
+        if (w > h && w > ww) {// 如果宽度大的话根据宽度固定大小缩放
+            be = (int) (newOpts.outWidth / ww);
+        } else if (w < h && h > hh) {// 如果高度高的话根据宽度固定大小缩放
+            be = (int) (newOpts.outHeight / hh);
+        }
+        if (be <= 0)
+            be = 1;
+        newOpts.inSampleSize = be;// 设置缩放比例
+        //newOpts.inScaled=false;
+        // 重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        bitmap = BitmapFactory.decodeFile(srcPath, newOpts);
+        return new ReturnObject(newOpts, compressImage(bitmap));
+    }
+
+    //一个方法返回多个对象
+    public static class ReturnObject {
+        public BitmapFactory.Options options;
+        public Bitmap bitmap;
+
+        public ReturnObject(BitmapFactory.Options options, Bitmap bitmap) {
+            this.options = options;
+            this.bitmap = bitmap;
+        }
     }
 
 
